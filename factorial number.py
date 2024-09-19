ddcad3621a2f6d58fee1cb66b8fdd57608f429a5
hnani46 <22hr1a3246@mtieat.org>
2024-09-19 14:13:14 +0530

    Create factorial number.py

---
+++ factorial number.py
@@ -0,0 +1,7 @@
+.....
+Write a program to determine whether 'n' is a factorial number or not. Factorial of a number is the product of all positive numbers from 1 to 'n'.
+....
+Sample Input:
+6
+Sample Output:
+Yes
