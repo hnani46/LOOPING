a544a7f678a101405d6a1cef0b4af558242292a5
hnani46 <22hr1a3246@mtieat.org>
2024-09-19 14:27:58 +0530

    Create trendy number.py

---
+++ trendy number.py
@@ -0,0 +1,7 @@
+.....
+Write a program to check whether the given number is a trendy number or not. A number is said to be a trendy number if and only if it has 3 digits and the middle digit is divisible by 3.
+...
+Sample Input:
+791
+Sample Output:
+Trendy Number
