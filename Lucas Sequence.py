8d680831fb4ddee41acd821dc06cf09f0b465448
hnani46 <22hr1a3246@mtieat.org>
2024-09-19 14:24:31 +0530

    Update Lucas Sequence.py

--- Lucas Sequence.py
+++ Lucas Sequence.py
@@ -5,3 +5,19 @@ Sample Input:
 5
 Sample Output:
 0 0 1 1 2
+....
+Ans:
+n=int(input())
+a=0
+b=0
+c=1
+print(a,end=’ ‘)
+print(b,end=’ ‘)
+print(c,end=’ ‘)
+for i in range (4,n+1):
+	d=a+b+c
+	a=b
+	b=c
+	c=d
+	print(d,end=’ ‘)
+
